15a2e86f08471f5ff7f5e4af5a4e5a95732690a9
RayShade <egor.malyshev@gmail.com>
2015-03-10 16:06:05 +0300

    Misc fixes and improvements

--- src/jetbrains/macros/util/Strings.java
+++ src/jetbrains/macros/util/Strings.java
@@ -13,14 +13,14 @@ public class Strings {
     public static final String REST_PREFIX = "/rest/";
     public static final String PROJECT = "project";
     public static final String QUERY = "query";
-    public static final String BODY_REPORT = "templates/body-report.vm";
-    public static final String ROW = "templates/body-report-row.vm";
+    public static final String TEMPLATES_ROOT = "templates/";
+    public static final String BODY_REPORT = TEMPLATES_ROOT + "body-report.vm";
+    public static final String ROW = TEMPLATES_ROOT + "body-report-row.vm";
     public static final String DETAILED = "detailed";
     public static final String SHORT = "short";
-    public static final String BODY_DETAILED = "templates/body-link-detailed.vm";
-    public static final String BODY_LINK = "templates/body-link.vm";
+    public static final String BODY_DETAILED = TEMPLATES_ROOT + "body-link-detailed.vm";
+    public static final String BODY_LINK = TEMPLATES_ROOT + "body-link.vm";
     public static final String ERROR = "error";
-    public static String USER = "user";
     public static String STYLE = "style";
     public static String ID = "id";
 }
