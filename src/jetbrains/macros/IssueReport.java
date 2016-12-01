2ef72bd898884b3c3ad2a41a56f3a8f8f096e27d
RayShade <egor.malyshev@gmail.com>
2016-12-01 13:43:53 +0300

    Version inc

--- src/jetbrains/macros/IssueReport.java
+++ src/jetbrains/macros/IssueReport.java
@@ -16,7 +16,7 @@ import jetbrains.macros.util.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import youtrack.Issue;
-import youtrack.issue.fields.BaseIssueField;
+import youtrack.issues.fields.BaseIssueField;
 
 import javax.servlet.http.HttpServletRequest;
 import java.text.MessageFormat;
